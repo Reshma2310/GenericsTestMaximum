456ed7e3b36cbb3e06239c12578119a19b7dcbd5
Reshma2310 <dreshma253@gmail.com>
2022-05-28 14:46:51 +0500

    [Reshma] - Maximum Value in Second Place - Test Case

--- GenericsTestMaximum/TestingGenericMax/UnitTest1.cs
+++ GenericsTestMaximum/TestingGenericMax/UnitTest1.cs
@@ -10,12 +10,13 @@ namespace TestingGenericMax
             var ActualResult = max.MaximumOfAll(a, b, c);
             Assert.AreEqual(a, ActualResult);
         }
+        [Test]
         public void MaxValueInSecondPosition_CheckMaxNumber_ReturnMaxInSecondPosition()
         {
             int a = 25, b = 40, c = 15;
             GenericsTestMaximum.MaximumValue<int> max = new GenericsTestMaximum.MaximumValue<int>();
             var ActualResult = max.MaximumOfAll(a, b, c);
             Assert.AreEqual(b, ActualResult);
-        }
+        }        
     }
 }
