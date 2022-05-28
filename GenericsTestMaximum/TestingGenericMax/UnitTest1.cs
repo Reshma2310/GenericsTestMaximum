4cde2c9712414c130d1157bf3c6c85c4ad4bbdc3
Reshma2310 <dreshma253@gmail.com>
2022-05-28 14:57:15 +0500

    [Reshma] - Maximum Value in Third positon - Test Case 3

--- GenericsTestMaximum/TestingGenericMax/UnitTest1.cs
+++ GenericsTestMaximum/TestingGenericMax/UnitTest1.cs
@@ -17,6 +17,14 @@ namespace TestingGenericMax
             GenericsTestMaximum.MaximumValue<int> max = new GenericsTestMaximum.MaximumValue<int>();
             var ActualResult = max.MaximumOfAll(a, b, c);
             Assert.AreEqual(b, ActualResult);
-        }        
+        }
+        [Test]
+        public void MaxValueInThirdPosition_CheckMaxNumber_ReturnMaxInThirdPosition()
+        {
+            int a = 25, b = 20, c = 65;
+            GenericsTestMaximum.MaximumValue<int> max = new GenericsTestMaximum.MaximumValue<int>();
+            var ActualResult = max.MaximumOfAll(a, b, c);
+            Assert.AreEqual(c, ActualResult);
+        }
     }
 }
